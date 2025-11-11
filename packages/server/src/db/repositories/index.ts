dfcad1d168caa9f324aa4ba67c6e73343f2a0fc5
cp-james-harbeck <jamesharbeck@cipherproxy.com>
2025-11-10 23:03:10 -0500

    Refactor DatabaseSystem into modular repository architecture
    Former-commit-id: a075a9f458b29aa332f3ffaa7ff919e0bdc8ba1d
    Former-commit-id: 12e1c20fa1f281233bf2a77c5cb80bd530553457
    Former-commit-id: 96c226c23d4a2a37409d50302c5e692df7ac41e8

---
+++ packages/server/src/db/repositories/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Repository Module - Barrel Export
+ *
+ * Exports all database repositories for easy import.
+ * Each repository handles a specific domain of database operations.
+ *
+ * Usage:
+ * ```typescript
+ * import { PlayerRepository, InventoryRepository } from './database/repositories';
+ * ```
+ */
+
+export { BaseRepository } from "./BaseRepository";
+export { CharacterRepository } from "./CharacterRepository";
+export { PlayerRepository } from "./PlayerRepository";
+export { InventoryRepository } from "./InventoryRepository";
+export { EquipmentRepository } from "./EquipmentRepository";
+export { SessionRepository } from "./SessionRepository";
+export { WorldChunkRepository } from "./WorldChunkRepository";
+export { NPCKillRepository } from "./NPCKillRepository";
